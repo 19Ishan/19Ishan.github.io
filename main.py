290bf318de6f27768df2cc0b214662605c6a8c04
MOHAMMAD ISHAN ANWAR <94357547+19Ishan@users.noreply.github.com>
2022-05-09 05:01:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ st.write("""This is the python project
 
 
 """)
-image = Image.open("J:\PycharmProject\StockMarketAnalysis\Photo\stockpic.jpg")
+image = Image.open("https://github.com/19Ishan/19Ishan.github.io/blob/master/stockpic.jpg")
 st.image(image, use_column_width=True)
 st.sidebar.header('User Input')
 
@@ -37,17 +37,17 @@ def get_company_name(symbol):#getting the company name
 
 def get_data(symbol, start, end):#here we are getting company data from the start date to the end date
     if symbol.upper() == 'AMZN':
-        df = pd.read_csv("J:\PycharmProject\StockMarketAnalysis\Stocks\gamzn.csv")#copy path  for amzn.csv
+        df = pd.read_csv("https://github.com/19Ishan/19Ishan.github.io/blob/master/Stocks/gamzn.csv")#copy path  for amzn.csv
     elif symbol.upper() == 'TSLA':
-        df = pd.read_csv("J:\PycharmProject\StockMarketAnalysis\Stocks\gtsla.csv")#copy path  for tsla.csv
+        df = pd.read_csv("https://github.com/19Ishan/19Ishan.github.io/blob/master/Stocks/gtsla.csv")#copy path  for tsla.csv
     elif symbol.upper() == 'GOOG':
-        df = pd.read_csv("J:\PycharmProject\StockMarketAnalysis\Stocks\goog.csv")#copy path  for Goog.csv
+        df = pd.read_csv("https://github.com/19Ishan/19Ishan.github.io/blob/master/Stocks/goog.csv")#copy path  for Goog.csv
     elif symbol.upper() == 'TCS':
-        df = pd.read_csv("J:\PycharmProject\StockMarketAnalysis\Stocks\gtcs.csv")#copy path  for tcs.csv
+        df = pd.read_csv("https://github.com/19Ishan/19Ishan.github.io/blob/master/Stocks/gtcs.csv")#copy path  for tcs.csv
     elif symbol.upper() == 'AIRTEL':
-        df = pd.read_csv("J:\PycharmProject\StockMarketAnalysis\Stocks\gartl.csv")#copy path  for airtel.csv
+        df = pd.read_csv("https://github.com/19Ishan/19Ishan.github.io/blob/master/Stocks/gartl.csv")#copy path  for airtel.csv
     elif symbol.upper() == 'WIPRO':
-        df = pd.read_csv("J:\PycharmProject\StockMarketAnalysis\Stocks\gwpro.csv")#copy path  for wipro.csv
+        df = pd.read_csv("https://github.com/19Ishan/19Ishan.github.io/blob/master/Stocks/gwpro.csv")#copy path  for wipro.csv
     else:
         df = pd.DataFrame(columns=['Date','Close', 'Open', 'Volume', 'Adj Close', 'High', 'Low'])
 
